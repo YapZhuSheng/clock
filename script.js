d5615cd46c87dfbb8835a7ab736877b8fd40afd9
Zhu Sheng <yapzhusheng@gmail.com>
2024-01-24 13:58:17 +0800

    debloat

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ m = (m<10) ? "0"+ m : m;
 s = (s<10) ? "0"+ s : s;
 
 var time = h+":"+m+":"+s+" "+session;
-document.getElementById("MyCOCKDisplay").innerText = time;
+document.getElementById("clock").innerText = time;
 setTimeout(showTime,1000);
 }
 showTime();
